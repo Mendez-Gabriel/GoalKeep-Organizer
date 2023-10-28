2f14646f588cad81783a1b70295869e3fe105976
Gabriel Mendez <mendez.gabriel.d@hotmail.com>
2023-10-27 22:25:14 -0300

    se crea footer

---
+++ src/components/specific/footer/Footer.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+// const Footer = () => {
+//     return (
+//         <div>
+//             <ul className="nav justify-content-center fixed-bottom bg-dark bg-gradient">
+//                 <li className="nav-item">
+//                     <a className="nav-link active" aria-current="page" href="#">Active</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link" href="#">Link</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link" href="#">Link</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+//                 </li>
+//             </ul>
+//         </div>
+//     )
+// }
+
+// export default Footer;
