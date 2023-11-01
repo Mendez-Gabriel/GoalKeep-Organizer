28c051c23fdc3fcd9aab567140f6a35ad87b2d3f
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2023-11-01 12:05:54 -0300

    correccion en componente FootballFieldCard

--- src/components/specific/footballFieldCard/FootballFieldCard.jsx
+++ src/components/specific/footballFieldCard/FootballFieldCard.jsx
@@ -3,7 +3,6 @@ import styles from './FootballFieldCard.module.css'
 
 function FootballFieldCard() {
   return (
-    <section className='container-fluid row p-5 mt-5 mx-0 justify-content-center w-100'>
     <div className ={`col-10 col-md-5 col-lg-4 ${styles.cardContainer}`}>
       <img src="https://www.cariverplate.com.ar/imagenes/contenidos/2022-07/22070-1.jpg" alt="" />
       <div className={styles.info}>
@@ -14,7 +13,6 @@ function FootballFieldCard() {
       <div className={styles.overlay}></div>
       <div className={styles.button}><a href="#"> Ver Disponibilidad </a></div>
     </div>
-    </section>
   )
 }
 
