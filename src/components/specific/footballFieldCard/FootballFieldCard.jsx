caa13ab57b6f1744ff04b04b6e70a8ebe1d14133
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2023-11-02 18:50:38 -0300

    Se implementan props al componente y se corrijen algunos estilos

--- src/components/specific/footballFieldCard/FootballFieldCard.jsx
+++ src/components/specific/footballFieldCard/FootballFieldCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './FootballFieldCard.module.css'
 
-function FootballFieldCard() {
+function FootballFieldCard({  name, grassType, players, imgUrl }) {
   return (
-    <div className ={`col-10 col-md-5 col-lg-4 ${styles.cardContainer}`}>
-      <img src="https://www.cariverplate.com.ar/imagenes/contenidos/2022-07/22070-1.jpg" alt="" />
+    <div className ={`col-10 col-md-5 p-0 ${styles.cardContainer}`}>
+      <img src={ imgUrl } alt="img-estadio" />
       <div className={styles.info}>
-        <h3>monumental</h3>
-        <p>Jugadores: 11</p>
-        <p>cesped: Mixto europeo</p>
+        <h3>{ name }</h3>
+        <p>Jugadores: { players }</p>
+        <p>cesped: { grassType }</p>
       </div>
       <div className={styles.overlay}></div>
       <div className={styles.button}><a href="#"> Ver Disponibilidad </a></div>
