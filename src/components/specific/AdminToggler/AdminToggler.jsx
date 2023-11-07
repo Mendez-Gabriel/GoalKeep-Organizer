ec39fa9e74f2c9d0ec3e9f4e9015665a473fa1ee
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2023-11-07 19:44:45 -0300

    Cambios en estilos e iconos

--- src/components/specific/AdminToggler/AdminToggler.jsx
+++ src/components/specific/AdminToggler/AdminToggler.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import Styles from './AdminToggler.module.css';
+import { GearFill, Gear } from 'react-bootstrap-icons';
 const { container, tabs, tab, notification, glider } = Styles;
 
 const AdminToggler = ({ handleViewChange }) => {
@@ -18,16 +19,16 @@ const AdminToggler = ({ handleViewChange }) => {
       <div className={tabs}>
         <input type="radio" id="footballFields" name="tabs" 
         checked={viewOption==='footballFields'} onChange={handleRadioChange}/>
-        <label className={tab} htmlFor="footballFields">Canchas<span className={notification}>⚙️</span></label>
+        <label className={tab} htmlFor="footballFields">Canchas<span className={notification}><Gear color='#2E8B57'/></span></label>
         <input type="radio" id="users" name="tabs"
         checked={viewOption==='users'} onChange={handleRadioChange}/>
-        <label className={tab} htmlFor="users">Usuarios<span className={notification}>⚙️</span></label>
+        <label className={tab} htmlFor="users">Usuarios<span className={notification}><Gear color='#2E8B57'/></span></label>
         <input type="radio" id="reservations" name="tabs"
         checked={viewOption==='reservations'} onChange={handleRadioChange}/>
-        <label className={tab} htmlFor="reservations">Reservas<span className={notification}>⚙️</span></label>
+        <label className={tab} htmlFor="reservations">Reservas<span className={notification}><Gear color='#2E8B57'/></span></label>
         <input type="radio" id="products" name="tabs"
         checked={viewOption==='products'} onChange={handleRadioChange}/>
-        <label className={tab} htmlFor="products">Productos<span className={notification}>⚙️</span></label>
+        <label className={tab} htmlFor="products">Productos<span className={notification}><Gear color='#2E8B57'/></span></label>
         <span className={glider}></span>
       </div>
     </div>
