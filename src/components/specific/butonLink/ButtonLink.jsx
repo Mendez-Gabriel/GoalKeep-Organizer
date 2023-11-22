ae7715349a907c6257896c0b02bbb06fec4f6b26
Gabriel Mendez <mendez.gabriel.d@hotmail.com>
2023-11-22 00:59:54 -0300

    Solucion de ocultar el NavBar Mobile

--- src/components/specific/butonLink/ButtonLink.jsx
+++ src/components/specific/butonLink/ButtonLink.jsx
@@ -5,8 +5,8 @@ const ButtonLink = ({ Text, link, className, dismiss, handlerClick, click }) => 
 
   return (
       <div>
-          <li className="nav-item mx-3 py-2">
-              <Link to={link} className={`nav-link active ${className} text-light`} data-bs-dismiss={dismiss} onClick={click}>{Text}</Link>
+          <li className="nav-item mx-3 py-2 d-flex">
+              <Link to={link} className={`nav-link active ${className} text-light`} data-bs-dismiss={dismiss} onClick={click}><strong className='d-flex align-middle'>{Text}</strong></Link>
           </li>
       </div>
   )
--- src/components/specific/navbar/NavBar.jsx
+++ src/components/specific/navbar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logoIcon from '../../../assets/Icon/Icon.svg';
 import styleNavBar from './NavBar.module.css';
@@ -7,6 +7,7 @@ import BottonTonggler from '../../specific/bottonTonggler/BottonTonggler';
 import ButtonLink from '../butonLink/ButtonLink';
 import Dropdown from '../dropdown/Dropdown';
 import { House, Flag, Shop, CardImage, InfoCircle, Phone, PersonFill } from 'react-bootstrap-icons';
+import Offcanvas from 'react-bootstrap/Offcanvas'
 import ModalM from '../modal/ModalM';
 import Button from 'react-bootstrap/Button';
 
@@ -14,19 +15,26 @@ import Button from 'react-bootstrap/Button';
 
 const NavBar = ({ setUser, user }) => {
 
-    const [show, setShow] = useState(false);
-    const [click, setClick] = useState(false);
-    const [dismiss, setDismiss] = useState(false)
+    const { logoIconStyle, navicon, open } = styleNavBar;
+    const containerIcon = 'container d-flex p-2'
 
-    const handleDismiss = () => {
-        if(!dismiss){
-            setDismiss(true)
-        }else{
-            setDismiss(false)
-        }
+    const [show, setShow] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [tonggler, setTonggler] = useState()
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+        setTonggler(true)
     }
+
+
     const handleShow = () => setShow(true);
-    const handleClose = () => setShow(false);
+    const handleClose = () => {
+        setShow(false)
+        setMenuOpen(false)
+        setTonggler(false)
+    };
+
     const handleLogout = () => {
         localStorage.removeItem('user');
         setShow(false);
@@ -34,78 +42,71 @@ const NavBar = ({ setUser, user }) => {
     };
 
 
-
-    const handlerClick = () => {
-        setClick(!click)
-    };
-
-    const { logoIconStyle } = styleNavBar;
-    const conteinerIcon = 'd-flex align-items-center my-2';
-
     return (
         <header>
+            <Offcanvas show={menuOpen} onHide={handleClose} responsive="lg" className='bg-dark d-lg-none'>
+                <Offcanvas.Header closeButton>
+                    <div className={containerIcon}>
+                        <img src={logoIcon} alt="logoIcon" id={logoIconStyle} />
+                        <ul className='navbar-nav'>
+                            <ButtonLink Text={user?.loginUser.userPasswordHidden.userName} link={'/login'} className={'fs-3'} />
+                        </ul>
+                    </div>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <div className={containerIcon}>
+                        <House color='#919847' size={30} />
+                        <ButtonLink Text={'Home'} link={'/'} click={() => handleShowCanvas(showCanvas)} />
+                    </div>
+                    <div className={containerIcon}>
+                        <Flag color='#919847' size={30} />
+                        <ButtonLink Text={'Canchas'} link={'/canchas'} click={() => handleShowCanvas(showCanvas)} />
+                    </div>
+                    <div className={containerIcon}>
+                        <Shop color='#919847' size={30} />
+                        <ButtonLink Text={'Productos'} link={'/products'} click={() => handleShowCanvas(showCanvas)} />
+                    </div>
+                    <div className={containerIcon}>
+                        <CardImage color='#919847' size={30} />
+                        <ButtonLink Text={'Galeria'} link={'/galeria'} click={() => handleShowCanvas(showCanvas)} />
+                    </div>
+                    {user?.loginUser.userPasswordHidden.admin ?
+                        (
+                            <div className={containerIcon}>
+                                <PersonFill color='#919847' size={30} />
+                                <ButtonLink Text={'Administrador'} link={'/admin'} click={() => handleShowCanvas(showCanvas)} />
+                            </div>
+                        ) : ('')
+                    }
+
+                    <div className={containerIcon}>
+                        <InfoCircle color='#919847' size={30} />
+                        <ButtonLink Text={'Sobre Nosotros'} link={'/about'} click={() => handleShowCanvas(showCanvas)} />
+                    </div>
+                    <div className={containerIcon}>
+                        <Phone color='#919847' size={30} />
+                        <ButtonLink Text={'Contacto'} link={'/contactos'} click={() => handleShowCanvas(showCanvas)} />
+                    </div>
+                    <div className='mt-5'>
+                        {user ?
+                            (<Button variant="danger" onClick={handleShow}>Cerrar Sesion</Button>)
+                            :
+                            (<div className="btn-group me-2" role="group" aria-label="Second group">
+                                <Link to={'/user/login'} className='btn btn-success'>Inicia Sesion</Link>
+                                <Link to={'user/register'} className='btn btn-warning'>Registrate</Link>
+                            </div>)}
+                        <ModalM show={show} onClickCancel={handleClose} onClickClose={handleLogout} onHide={handleClose} />
+                    </div>
+                </Offcanvas.Body>
+            </Offcanvas>
             <nav className="navbar navbar-expand-lg fixed-top bg-dark bg-gradient">
                 <div className="container-fluid">
                     <img src={logoIcon} alt="logoIcon" id={logoIconStyle} />
-                    <BottonTonggler handlerClick={handlerClick} click={click}
-                        offcanvasHeader={
-                            <>
-                                <div className={conteinerIcon}>
-                                    <img src={logoIcon} alt="logoIcon" id={logoIconStyle} />
-                                    <ul className='navbar-nav'>
-                                        <ButtonLink Text={user?.loginUser.userPasswordHidden.userName} link={'/login'} className={'fs-3'} />
-                                    </ul>
-                                </div>
-                            </>
-                        }
-                        offcanvas={
-                            <>
-                                <div className={conteinerIcon}>
-                                    <House color='#919847' size={30} />
-                                    <ButtonLink Text={'Home'} link={'/'} />
-                                </div>
-                                <div className={conteinerIcon}>
-                                    <Flag color='#919847' size={30} />
-                                    <ButtonLink Text={'Canchas'} link={'/canchas'} />
-                                </div>
-                                <div className={conteinerIcon}>
-                                    <Shop color='#919847' size={30} />
-                                    <ButtonLink Text={'Productos'} link={'/products'} />
-                                </div>
-                                <div className={conteinerIcon}>
-                                    <CardImage color='#919847' size={30} />
-                                    <ButtonLink Text={'Galeria'} link={'/galeria'} />
-                                </div>
-                                {user?.loginUser.userPasswordHidden.admin ?
-                                    (
-                                        <div className={conteinerIcon}>
-                                            <PersonFill color='#919847' size={30} />
-                                            <ButtonLink Text={'Administrador'} link={'/admin'}  />
-                                        </div>
-                                    ) : ('')
-                                }
-
-                                <div className={conteinerIcon}>
-                                    <InfoCircle color='#919847' size={30} />
-                                    <ButtonLink Text={'Sobre Nosotros'} link={'/about'}  />
-                                </div>
-                                <div className={conteinerIcon}>
-                                    <Phone color='#919847' size={30} />
-                                    <ButtonLink Text={'Contacto'} link={'/contactos'}/>
-                                </div>
-                                <div className='mt-5'>
-                                    {user ?
-                                        (<Button variant="danger" onClick={handleShow}>Cerrar Sesion</Button>)
-                                        :
-                                        (<div className="btn-group me-2" role="group" aria-label="Second group">
-                                            <Link to={'/user/login'} className='btn btn-success'>Inicia Sesion</Link>
-                                            <Link to={'user/register'} className='btn btn-warning'>Registrate</Link>
-                                        </div>)}
-                                    <ModalM show={show} onClickCancel={handleClose} onClickClose={handleLogout} onHide={handleClose} />
-                                </div>
-                            </>
-                        }
-                    />
+                    <Button variant="" className={`d-lg-none ${navicon} ${tonggler ? open : ''}`} onClick={toggleMenu}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </Button>
                     <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
                         <ul className="navbar-nav ">
                             <ButtonLink Text={'Home'} link={'/'} />
