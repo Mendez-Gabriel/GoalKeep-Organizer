4243ddb23685a79fdde5d16f0cee435e0d78c3e5
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2023-11-27 16:56:01 -0300

    hotfix navbar font weight

--- src/components/specific/butonLink/ButtonLink.jsx
+++ src/components/specific/butonLink/ButtonLink.jsx
@@ -6,7 +6,7 @@ const ButtonLink = ({ Text, link, className, dismiss, handlerClick, click }) => 
   return (
       <div>
           <li className="nav-item mx-3 py-2 d-flex">
-              <Link to={link} className={`nav-link active ${className} text-light`} data-bs-dismiss={dismiss} onClick={click}><strong className='d-flex align-middle'>{Text}</strong></Link>
+              <Link to={link} className={`nav-link active ${className} text-light`} data-bs-dismiss={dismiss} onClick={click}><strong style={{fontWeight:'400' }} className='d-flex align-middle fst-italic'>{Text}</strong></Link>
           </li>
       </div>
   )
