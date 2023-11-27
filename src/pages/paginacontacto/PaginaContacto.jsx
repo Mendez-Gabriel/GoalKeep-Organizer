cc01803f78042f9f580c6ef2f3a0ad7b52b97e54
Alejo-Duhart <alejoduhart14@gmail.com>
2023-11-27 18:20:38 -0300

    se realizo el formulario de contacto con envio de emails

--- src/pages/paginacontacto/PaginaContacto.jsx
+++ src/pages/paginacontacto/PaginaContacto.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from './PaginaContacto.module.css'
 
 const ContactPage = () => {
-  const {contactoDiv, contactContainer, contactHeading, contactIntro, contactInfo, infoItem, infoHeading, infoSection} = style
+  const {contactoDiv, contactContainer, contactHeading, contactIntro, contactInfo, infoItem, infoHeading, infoSection,
+    FormButton} = style
 
   return (
     <div className={contactoDiv}>
       <div className={contactContainer}>
       <h1 className={contactHeading}>Contacto</h1>
       <p className={contactIntro}>¡Estamos encantados de escucharte! Puedes contactarnos utilizando la información de contacto a continuación:</p>
+      <Link to="/FormContacto">
+            <button className={FormButton}>Accede a un Formulario</button>
+      </Link> 
       <div className={contactInfo}>
         <h2 className={infoHeading}>Información de contacto</h2>
         <p className={infoItem}>Correo electrónico: bytecoderolling@gmail.com</p>
