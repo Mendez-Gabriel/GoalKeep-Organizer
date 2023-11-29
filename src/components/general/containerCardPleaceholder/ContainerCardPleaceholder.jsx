dc590a62a103b3d4f23756ead53e1f15ae041c50
Gabriel Mendez <mendez.gabriel.d@hotmail.com>
2023-11-29 16:18:35 -0300

    hot-fix

--- src/components/general/containerCardPleaceholder/ContainerCardPleaceholder.jsx
+++ src/components/general/containerCardPleaceholder/ContainerCardPleaceholder.jsx
@@ -3,7 +3,7 @@ import CardPleaseholder from '../cardPleaseholder/CardPleaseholder';
 
 const ContainerCardPleaceholder = () => {
   return (
-    <div className='d-flex flex-wrap justify-content-center border border-primary'>
+    <div className='d-flex flex-wrap justify-content-center'>
         <CardPleaseholder />
         <CardPleaseholder />
         <CardPleaseholder />
